dca616365c2111d15c796a5175c95764b2117839
Madis Kaaret <madiskrt@gmail.com>
2017-03-17 14:04:51 +0200

    yt results

--- js/youtube.js
+++ js/youtube.js
@@ -59,7 +59,54 @@ function search(){
 
 					//display results
 					$('#results').append(output);
-				}
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				//display buttons
+				$('#buttons').append(buttons);
 			}
 		);
+}
+
+//build output
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+	//build output string
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3>'+title+'</h3>' +
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clerfix"></div>' +
+	'';
+
+	return output;
+}
+
+//build the buttons
+function getButtons(prevPageToken, nextPageToken){
+	if(!prevPageToken){
+		var btnoutput = '<div class="button-container">' +
+			'<button id="next-button" class="paging-button" data-token="' +nextPageToken+ '" data-query="'+q+'"' +
+			'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">' +
+			'<button id="next-button" class="paging-button" data-token="' +prevPageToken+ '" data-query="'+q+'"' +
+			'onclick="prevPage();">Previous Page</button>' +
+			'<button id="next-button" class="paging-button" data-token="' +nextPageToken+ '" data-query="'+q+'"' +
+			'onclick="nextPage();">Next Page</button></div>';
+	}
+
+	return btnoutput;
 }
